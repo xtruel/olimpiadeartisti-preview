ad27d307f609615c688a4c2ade26635ce57d91db
bnb-Calabria <deploy@bnb-calabria.local>
2026-01-01 20:16:44 +0000

    Aggiornamento finale Navbar e fix pathing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xtruel.github.io',
   base: '/olimpiadeartisti-preview',
   output: 'static',
   vite: {
